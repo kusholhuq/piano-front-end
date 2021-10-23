456476efbef168cc90711e83bc717acbe1bfa89f
Mohsinul Huq <kusholhuq@gmail.com>
2021-10-23 02:27:45 -0700

    keys take a note prop

--- src/components/Key.js
+++ src/components/Key.js
@@ -5,6 +5,9 @@ class Key extends React.Component {
   render() {
     return (
       <div className="key">
+        <div className="key-text">
+          {this.props.note.toUpperCase()}
+        </div>
       </div>
     )
   }
