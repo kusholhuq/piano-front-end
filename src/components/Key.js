8493f78264cbcf7e5f80e44171b81dd235a4bb4c
Mohsinul Huq <kusholhuq@gmail.com>
2021-10-24 20:10:39 -0700

    flats are shown as black keys

--- src/components/Key.js
+++ src/components/Key.js
@@ -2,14 +2,36 @@ import React from 'react';
 import '../styles/Key.css';
 
 class Key extends React.Component {
+
+  noteIsFlat = (note) => {
+    return note.length > 1;
+  }
+
   render() {
-    return (
-      <div className="key">
-        <div className="key-text">
-          {this.props.note.toUpperCase()}
+
+    let keyClassName = "key";
+
+    const noteIsFlat = this.noteIsFlat(this.props.note);
+    if(noteIsFlat) {
+      keyClassName += " flat";
+    }
+
+    let key;
+    if (noteIsFlat) {
+      key = (
+        <div className={keyClassName}></div>
+      )
+    } else {
+      key = (
+        <div className={keyClassName}>
+          <div className="key-text">
+            {this.props.note.toUpperCase()}
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
+
+    return key;
   }
 }
 
