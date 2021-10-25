41fb2a6788e3108e112157f1b988dbb28a3246e5
Mohsinul Huq <kusholhuq@gmail.com>
2021-10-24 20:33:48 -0700

    key and note index constants added

--- src/global/constants.js
+++ src/global/constants.js
@@ -2,4 +2,37 @@ const NOTES = [
   'c', 'df', 'd', 'ef', 'e', 'f', 'gf', 'g', 'af', 'a', 'bf', 'b'
 ];
 
+const KEY_TO_NOTE = {
+  z:"c",
+  s:"df",
+  x:"d",
+  d:"ef",
+  c:"e",
+  v:"f",
+  g:"gf",
+  b:"g",
+  h:"af",
+  n:"a",
+  j:"bf",
+  m:"b"
+}
+
+const NOTE_TO_KEY = {
+  c  : 'z',
+  df : 's',
+  d  : 'x',
+  ef : 'd',
+  e  : 'c',
+  f  : 'v',
+  gf : 'g',
+  g  : 'b',
+  af : 'h',
+  a  : 'n',
+  bf : 'j',
+  b  : 'm'
+}
+
+
+
+
 export { NOTES };
