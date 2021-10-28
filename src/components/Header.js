4a25198130f70c76e2108810581f333367126b77
Mohsinul Huq <kusholhuq@gmail.com>
2021-10-27 23:06:28 -0700

    melody logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,13 @@ export default function Header(){
   return (
     <div className="navbar">
       <div className="half">
-        <div>Melody</div>
-        <div><i class="fas fa-ruler-horizontal"></i></div>
+        <i class="fa fa-keyboard-o mr-2 fa-3x" aria-hidden="true"></i>
+        <h2 className="pac mt-1">Melody</h2>
       </div>
       <div className="half justify-right">
         <div className="navbar-items">Piano Type</div>
         <div className="navbar-items">Landing Page</div>
       </div>
-
-
     </div>
-  )
+  );
 }
