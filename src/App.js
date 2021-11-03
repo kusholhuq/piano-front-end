ac333af8c6b64902c1d9b71160c2dd262689c00f
Mohsinul Huq <kusholhuq@gmail.com>
2021-11-02 21:15:28 -0700

    turned app into a class with state, and conditionally render different views

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import './App.css';
 import { Piano } from './components/Piano';
 import Header from './components/Header';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Header/>
-        <div className="piano-container">
-          <Piano />
-        </div>
+class App extends React.Component {
 
-      </header>
-    </div>
-  );
+  constructor(props){
+    super(props);
+    this.state = {
+      view: 'landing page'
+    }
+  }
+
+  setView = (screen) => {
+    this.setState({
+      view: screen
+    })
+  }
+
+  render() {
+    const {view} = this.state;
+    let currentView;
+    switch (view) {
+      case 'landing page':
+        currentView = <div>This is your landing page substitute</div>;
+        break;
+      case 'piano':
+        currentView = (
+          <div className="App">
+              <Header />
+            <div className="piano-container">
+              <Piano />
+            </div>
+          </div>
+        );
+        break;
+    }
+
+    return (
+      currentView
+    );
+  }
+
 }
 
 export default App;
