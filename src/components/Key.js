17f5ee4f9749b016f794efafc21b9e1ef335045f
Mohsinul Huq <kusholhuq@gmail.com>
2021-12-13 00:07:32 -0800

    no button variable

--- src/components/Key.js
+++ src/components/Key.js
@@ -14,9 +14,8 @@ class Key extends React.Component {
 
   render() {
 
-    const button = (
-      <span>{NOTE_TO_KEY[this.props.note]}</span>
-    )
+    const keyboardKey = NOTE_TO_KEY[this.props.note].toUpperCase();
+
 
     let keyClassName = "key";
 
@@ -32,13 +31,15 @@ class Key extends React.Component {
     let key;
     if (noteIsFlat) {
       key = (
-        <div className={keyClassName}></div>
+        <div className={keyClassName}>
+          <div className="button-flat">{keyboardKey}</div>
+        </div>
       )
     } else {
       key = (
         <div className={keyClassName}>
           <div className="key-text">
-            {this.props.note.toUpperCase()}
+            <div className="button">{keyboardKey}</div>
           </div>
         </div>
       )
