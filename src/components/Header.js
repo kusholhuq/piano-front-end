68ade83b283e753f3d8073677af9a5dbc37177b1
Mohsinul Huq <kusholhuq@gmail.com>
2021-12-28 20:48:46 -0800

    header portrait iphone

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import '../styles/Header.css';
 export default function Header(props){
   return (
     <div className="navbar">
-      <div className="half">
+      <div className="half bump-up">
         <i class="fa fa-keyboard-o mr-2 fa-3x" aria-hidden="true"></i>
         <h2 className="pac mt-1">Melody</h2>
       </div>
-      <div className="half justify-right">
+      <div className="half bump-up justify-right">
         <div
         onClick={() => { props.setView("piano") }}
         className="navbar-items"
